74d9a61dd11ca9471d711d33d1213afd398acca9
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-05-26 13:27:03 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = (env, argv) => {
     output: {
       filename: isProduction ? '[name].[contenthash].min.js' : 'index.js', // Output bundle filename
       path: path.resolve(__dirname, 'dist'), // Output directory for bundled files
-      publicPath: 'https://erichsia7.github.io/vector/dist/',
+      publicPath: './',
       library: {
         name: 'vector',
         type: 'umd',
