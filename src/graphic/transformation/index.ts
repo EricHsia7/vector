f376bde941a724cb907b4b20bb5342be31900895
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-05-26 13:35:06 +0800

    update

--- src/graphic/transformation/index.ts
+++ src/graphic/transformation/index.ts
@@ -44,7 +44,7 @@ export function createSkewYMatrix(skewY: skewY): matrix {
 }
 
 export function multiplyMatrices(a: matrix, b: matrix): matrix {
-  const result: matrix = [
+  var result: matrix = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
@@ -53,7 +53,7 @@ export function multiplyMatrices(a: matrix, b: matrix): matrix {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       for (let k = 0; k < 3; k++) {
-        result[i][j] += a[i][k] * b[k][j];
+        result[i][j] = result[i][j] + a[i][k] * b[k][j];
       }
     }
   }
