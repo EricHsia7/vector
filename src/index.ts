03ba2a53c9303f3092ff1c414ce11e5a68f789aa
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-05-26 15:07:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ window.vector = {
     transformPoints,
     r: function () {
       var plane0 = buildPlane(5, 40, 300, 300);
-      var rect0 = buildRect(10, 20, 50, 80, 15, 15, '#000');
+      var rect0 = buildRect(10, 20, 50, 80, 15, 15, '#000', null, null, null, null, null, [{ type: 'rotate', deg: 45 }], 0.7, null);
       plane0.elements.push(rect0);
       console.log(
         renderPlaneAsXML(plane0, {
