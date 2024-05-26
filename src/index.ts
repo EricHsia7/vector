a138c2b22132c63db962db566fa5b28beb0467ef
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-05-26 13:13:08 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { flattenTransformIntoMatrix } from './transformation/index.ts';
 
+window.vector = {
+  test: {
+    flattenTransformIntoMatrix
+  },
+  initialize: function () {
+    console.log(0);
+  }
+};
+
+export default window.vector;
