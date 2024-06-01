6c29230b8f20839d70f4bb2b4fb38029f895e43f
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-06-01 10:23:56 +0800

    update

--- src/new_document/index.ts
+++ src/new_document/index.ts
@@ -1,9 +1,12 @@
 const newDocumentElement: HTMLElement = document.querySelector('.css_new_document');
+const newDocumentModalOverlayElement: HTMLElement = document.querySelector('.css_new_document_modal_overlay');
 
 export function openNewDocument(): void {
   newDocumentElement.setAttribute('displayed', true);
+  newDocumentModalOverlayElement.setAttribute('displayed', true);
 }
 
 export function closeNewDocument(): void {
   newDocumentElement.setAttribute('displayed', false);
+  newDocumentModalOverlayElement.setAttribute('displayed', false);
 }
