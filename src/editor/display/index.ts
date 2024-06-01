0f0de8b67635f51d66949419acaf2cfe883fa967
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-06-01 17:58:18 +0800

    update

--- src/editor/display/index.ts
+++ src/editor/display/index.ts
@@ -1,4 +1,5 @@
 import { renderPlaneAsXML } from '../../graphic/render/index';
+import { editingVectorDocument } from '../index';
 import { addingShapePlane } from '../tools/add/shape.ts';
 
 export function renderAddingShapePlane(): void {
@@ -12,7 +13,7 @@ export function renderAddingShapePlane(): void {
 
 export function renderEditorPlane(): void {
   const addingEditorPlaneElement: HTMLElement = document.querySelector('.css_editor .css_editor_plane');
-  var xml: string = renderPlaneAsXML(addingShapePlane, {
+  var xml: string = renderPlaneAsXML(editingVectorDocument.planes[0], {
     flattenTransform: false,
     exceptID: false
   });
