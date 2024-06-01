336831841898c38a9c8b4f4d4fc8623c19e13edf
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-06-01 18:06:42 +0800

    update

--- src/new_document/index.ts
+++ src/new_document/index.ts
@@ -14,7 +14,7 @@ export function closeNewDocument(): void {
 }
 
 export function createDocumentWithInputs(): void {
-  const name: string = String(newDocumentElement.querySelector('#new_document_name')) || 'Untitled';
+  const name: string = String(newDocumentElement.querySelector('#new_document_name').value) || 'Untitled';
   const width: number = parseInt(newDocumentElement.querySelector('#new_document_width').value) || 64;
   const height: number = parseInt(newDocumentElement.querySelector('#new_document_height').value) || 64;
   var creation: vectorDocument = createVectorDocument(width, height, null, null, name);
