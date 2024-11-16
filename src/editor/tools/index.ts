b2f1a28605be2d5544bc6a48167db0872741f97c
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-11-16 23:44:36 +0800

    debug

--- src/editor/tools/index.ts
+++ src/editor/tools/index.ts
@@ -17,6 +17,7 @@ export let currentTool: tools = 'shape';
 export let selectedElements: Array<string> = [];
 
 export function initializeTools(): void {
+  console.log('a', 0);
   const eventsSet = [
     ['touchstart', 'touchmove', 'touchend', 'touchcancel'],
     ['mousedown', 'mousemove', 'mouseup', 'mouseout']
@@ -24,90 +25,138 @@ export function initializeTools(): void {
 
   const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
   const events = isTouchDevice ? eventsSet[0] : eventsSet[1];
+  console.log('a', 1);
 
   // Event listeners
   editorCanvasElement.addEventListener(events[0], (event) => {
+    console.log('b', 0);
     event.preventDefault(); // Prevent scrolling
+    console.log('b', 1);
+
     if (isTouchDevice) {
+      console.log('b', 2);
+
       const touch = event.touches[0];
       currentCursorX = touch.clientX;
       currentCursorY = touch.clientY;
       currentTouchPointIdentifier = touch.identifier * 1;
     } else {
+      console.log('b', 3);
+
       currentCursorX = event.offsetX;
       currentCursorY = event.offsetY;
     }
+    console.log('b', 4);
+
     switch (currentTool) {
       case 'shape':
+        console.log('b', 5);
+
         addShapeElement(currentCursorX, currentCursorY);
         break;
       case 'path':
+        console.log('b', 6);
+
         addPathElement(currentCursorX, currentCursorY);
         break;
       default:
         break;
     }
   });
+  console.log('a', 2);
 
   editorCanvasElement.addEventListener(events[1], (event) => {
+    console.log('c', 0);
+
     event.preventDefault(); // Prevent scrolling
     if (isTouchDevice) {
+      console.log('c', 1);
+
       var touches: [] = [];
       for (var t in event.touches) {
+        console.log('c', 2);
+
         if (event.touches.hasOwnProperty(t) && typeof event.touches[t] === 'object') {
+          console.log('c', 3);
+
           touches.push(event.touches[t]);
         }
       }
       const touch = touches.filter((p) => p.identifier === currentTouchPointIdentifier)[0];
       if (touch) {
+        console.log('c', 4);
+
         currentCursorX = touch.clientX;
         currentCursorY = touch.clientY;
       }
     } else {
+      console.log('c', 5);
+
       currentCursorX = event.offsetX;
       currentCursorY = event.offsetY;
     }
+    console.log('c', 6);
+
     switch (currentTool) {
       case 'shape':
+        console.log('c', 7);
+
         modifyAddingShapeElement(currentCursorX, currentCursorY);
         break;
       case 'path':
+        console.log('c', 8);
+
         modifyAddingPathElement(currentCursorX, currentCursorY);
         break;
       default:
         break;
     }
   });
+  console.log('a', 3);
 
   editorCanvasElement.addEventListener(events[2], (event) => {
+    console.log('d', 0);
+
     event.preventDefault(); // Prevent scrolling
     if (isTouchDevice) {
+      console.log('d', 1);
       var touches = [];
       for (var t in event.changedTouches) {
+        console.log('d', 2);
         if (event.changedTouches.hasOwnProperty(t) && typeof event.changedTouches[t] === 'object') {
+          console.log('d', 3);
+
           touches.push(event.changedTouches[t]);
         }
       }
       const touch = touches.filter((p) => p.identifier === currentTouchPointIdentifier)[0];
       if (touch) {
+        console.log('d', 4);
+
         currentCursorX = touch.clientX;
         currentCursorY = touch.clientY;
       }
     } else {
+      console.log('d', 5);
       currentCursorX = event.offsetX;
       currentCursorY = event.offsetY;
     }
+    console.log('d', 6);
     switch (currentTool) {
       case 'shape':
+        console.log('d', 7);
         settleAddingShapeElement(currentCursorX, currentCursorY);
         break;
       case 'path':
+        console.log('d', 8);
         settleAddingPathElement(currentCursorX, currentCursorY);
         break;
       default:
         break;
     }
   });
+  console.log('a', 4);
+
   /*
   canvas.addEventListener(events[3], (e) => {
     var cursorX: number = 0;
@@ -128,25 +177,7 @@ export function initializeTools(): void {
 }
 
 export function switchTool(tool: number): void {
-  console.log(0, currentTool);
-  switch (tool) {
-    case 0:
-      currentTool = 'selector';
-      break;
-    case 1:
-      currentTool = 'shape';
-      break;
-    case 2:
-      currentTool = 'pen';
-      break;
-    case 3:
-      currentTool = 'move';
-      break;
-    default:
-      break;
-  }
-  // currentTool = ['select', 'shape', 'path', 'move'][tool];
-  console.log(1, currentTool);
+  currentTool = ['select', 'shape', 'path', 'move'][tool];
   let index = 0;
   for (const toolbarButton of toolbarButtonElements) {
     if (index === tool) {
